bdc9237be3cb024b1c340c64dce845d33e5f6baf
Pedro Siqueira <pedro.siqueira1999@hotmail.com>
2023-11-28 21:05:24 -0300

    provider do Dio

---
+++ lib/src/core/providers/application_providers.dart
@@ -0,0 +1,10 @@
+
+
+
+import 'package:barbearia_tcc/src/core/rest_client/rest_client.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'application_providers.g.dart';
+
+@Riverpod(keepAlive: true)
+RestClient restClient(RestClientRef ref) => RestClient();
