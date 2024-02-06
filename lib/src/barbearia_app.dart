8cf59d271383dbdb2dd2101776c64b28bd03639a
Pedro Siqueira <pedro.siqueira1999@hotmail.com>
2024-02-05 22:56:20 -0300

    expire token

--- lib/src/barbearia_app.dart
+++ lib/src/barbearia_app.dart
@@ -1,5 +1,6 @@
 import 'package:asyncstate/asyncstate.dart';
 import 'package:barbearia_tcc/src/core/ui/barberaria_theme.dart';
+import 'package:barbearia_tcc/src/core/ui/barbershop_nav_global_key.dart';
 import 'package:barbearia_tcc/src/core/ui/widget/loader.dart';
 import 'package:barbearia_tcc/src/features/auth/login/login_page.dart';
 import 'package:barbearia_tcc/src/features/splash/splash_page.dart';
@@ -17,6 +18,7 @@ class BarbeariaApp extends StatelessWidget {
           title: 'Flutter Demo',
           theme: BarberariaTheme.themeData,
           navigatorObservers: [asyncNavigatorObserver],
+          navigatorKey: BarbershopNavGlobalKey.instance.navKey,
           routes: {
             '/': (_) => const SplashPage(),
             '/auth/login': (context) => const LoginPage(),
